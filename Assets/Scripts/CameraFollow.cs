899f266379a34a8e869ff830e8380dbcdc359332
Melisa Ronay Seven <melisar.seven@gmail.com>
2022-04-25 03:27:30 +0300

    feat: Added camera follow function

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private float lerpValue;
+
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 targetPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, lerpValue * Time.deltaTime);
+    }
+}
